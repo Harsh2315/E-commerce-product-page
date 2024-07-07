efe1bd7aa0947e31b89a73ef74b55b598ae132ec
Harsh2315 <hguptagwalior@gmail.com>
2024-07-07 15:27:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let ProductImage=document.getElementById("ProductImage");
+let Image=document.getElementsByClassName("img2");
+Image[0].onclick=function(){
+    ProductImage.src=Image[0].src;
+}
+Image[1].onclick=function(){
+    ProductImage.src=Image[1].src;
+}
+Image[2].onclick=function(){
+    ProductImage.src=Image[2].src;
+}
+Image[3].onclick=function(){
+    ProductImage.src=Image[3].src;
+}
+Image[4].onclick=function(){
+    ProductImage.src=Image[4].src;
+}
